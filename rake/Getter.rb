06dd2dde2cad765941827600c4c0d606d2b4761a
Belic Bojan <belic_bojan@yahoo.com>
2015-10-23 13:25:21 +0200

    Added OS-separation for Getter.rb

--- rake/Getter.rb
+++ rake/Getter.rb
@@ -6,14 +6,18 @@
 #
 #
 #
+require(File.join(ENV['APT_INIT'],'/rake/Cradle.rb'))
 
 class Getter
 
   extend Rake::DSL
 
   def self.update()
-    begin 
-      sh "sudo apt-get update -y"
+    begin
+      case Cradle.getOs
+	when 'Linux' then sh "sudo apt-get update -y"
+	when 'OSX' then puts "No updates for Brew" 	
+      end
     rescue => e
       puts "ERROR: Problems encountered during apt-get update!".red
       puts "#{e.class}: #{e.message}".red
@@ -22,8 +26,11 @@ class Getter
 
   def self.install(package)
     begin
-      sh "sudo apt-get install -y #{package}"
-    rescue => e
+      case Cradle.getOs
+	when 'Linux' then sh "sudo apt-get install -y #{package}" 
+	when 'OSX' then sh "brew install -y #{package}"
+      end
+      rescue => e
       puts "ERROR: Problems encountered during apt-get install!".red
       puts "#{e.class}: #{e.message}".red
     end
@@ -31,7 +38,10 @@ class Getter
 
   def self.addRepo(repo)
     begin
-      sh "sudo add-apt-repository -y #{repo}"
+      case Cradle.getOs
+	when 'Linux' then sh "sudo add-apt-repository -y #{repo}"
+	when 'OSX' then puts "No addRepo for OSX yet"
+      end
     rescue => e
       puts "ERROR: Problems encountered during add-apt-repository!".red
       puts "#{e.class}: #{e.message}".red
