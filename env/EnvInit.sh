9bc0f56f1a73935027593a3a16828181f7605f32
ItWillDo <belic_bojan@yahoo.com>
2016-12-06 23:34:05 +0100

    Cleaned up EnvInit

--- env/EnvInit.sh
+++ env/EnvInit.sh
@@ -2,29 +2,14 @@
 export REPO=~/REPO
 export APT_INIT=$REPO/APT-InIt
 export build_publish=/tmp/pub
-export gubg=$build_publish
-
-# EDU_REPO Variable -- EDU_REPO is used for minor test projects and educational purposes, to avoid cluttering REPO
-export EDU_REPO=/media/itwilldo/DATA/EDU_REPO
-
-# htdocs Variable -- Used as a quick reference to the 'htdocs' folder for website projects
-export HTDOCS=/opt/lampp/htdocs
 
 # APPS Variable -- Reference to applications folder
 export APPS=$HOME/Applications
 
-# Variable for building Android NDK
-export ndk_build=$HOME/Android/ndk/android-ndk-r10e/ndk-build
-export PATH=$PATH:$HOME/Android/sdk/platform-tools
-
-# Variable for JDK
-###export JAVA_HOME=/usr/local/jdk1.8.0_60
-#export JAVA_HOME=/usr/local/jdk1.7.0_80
-#export PATH=$PATH:$JAVA_HOME/bin
-
 # Terminal functions
-function refresh() { source ~/.zshrc }
-
+function refresh() {
+  source ~/.zshrc
+}
 
 # Git Difftool configuration
 ### in terminal use: 'git df ${FILENAME}'
@@ -37,27 +22,30 @@ git config --global diff.tool gvimdiff;
 git config --global difftool.prompt false
 git config --global alias.df difftool
 
-
 # Git Functions
-function gstat() {git status}
-function gdf() {git df $@} # $@ gives all input arguments separated by spaces
-function gall() {git add --all}
-function gcheck() {git checkout $1}
-function gcom() {git commit -m $1}
-function gresh() {git reset --hard}
+function gstat() {
+  git status
+}
 
-# TaskWarrior functions
-alias ta="task add"
-alias td="task done"
-alias tl="task list"
+function gdf() {
+  git df $@ # $@ gives all input arguments separated by spaces
+}
+
+function gall() {
+  git add --all
+}
+
+function gcheck() {
+  git checkout $1
+}
+
+function gcom() {
+  git commit -m $1
+}
+
+function gresh() {
+  git reset --hard
+}
 
 # Add 'exe'-folder to path
 export PATH=$APT_INIT/env/exe:$PATH
-
-# Build compiler functions
-function setBC_release() { export build_compiler=clang7_x64-release }
-function setBC_debug() { export build_compiler=clang7_x64-debug }
-
-
-# Autocomplete for files
-#complete -f gv
